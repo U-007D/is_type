add59326ce7ee297b07c531bed98923f8a725854
bRad <brad@humanenginuity.com>
2017-05-28 15:29:09 -0700

    function_syntax_identifies_some_enum_as_some: pass; function_syntax_rejects_none_enum_as_some

--- src/lib.rs
+++ src/lib.rs
@@ -4,15 +4,10 @@
 #![warn(missing_debug_implementations, missing_copy_implementations, trivial_casts, trivial_numeric_casts, unused_import_braces, unused_qualifications)]
 #![deny(unused_must_use, overflowing_literals)]
 
-type GeneralError = Box<std::error::Error>;
-type GeneralResult<T> = std::result::Result<T, GeneralError>;
-
-mod consts;
-use consts::msgs;
+// Currently implements is_enum!(). TODO: Further explore Rust to generalize this macro to truly implement is_type!()
+macro_rules! is_type {
+    ($value:expr, $type_name:pat) => { if let $type_name = $value { true } else { false } };
+}
 
 #[cfg(test)]
 mod unit_tests;
-
-pub fn lib_main(_args: Vec<String>) -> GeneralResult<()> {
-    Ok(())
-}
--- src/unit_tests.rs
+++ src/unit_tests.rs
@@ -1,7 +1,19 @@
-use super::*;
-
 #[test]
 fn empty_test() {}
 
 #[test]
-fn t() {}
+fn function_syntax_accepts_some_enum_as_some() {
+    let input = Some(42u8);
+    let expected_result = true;
+
+    assert!(is_type!(input, Some(_)) == expected_result);
+}
+
+#[test]
+fn function_syntax_rejects_none_enum_as_some() {
+    #[allow(unused_qualifications)] //silences incorrect warning
+    let input = Option::None::<u8>;
+    let expected_result = false;
+
+    assert!(is_type!(input, Some(_)) == expected_result);
+}
